d542df886acd0ebfae313e93c318b1d305873fa9
boldi24 <boldizsartompe@gmail.com>
2020-01-25 14:34:11 +0000

    Add new XMLFormatter

--- src/main/java/utils/XmlFormatter.java
+++ src/main/java/utils/XmlFormatter.java
@@ -1,38 +1,31 @@
 package utils;
 
-//import com.sun.org.apache.xml.internal.serialize.OutputFormat;
-//import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import exception.XmlPrettifyException;
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.io.Writer;
 
 public class XmlFormatter {
 
     public static String prettify(String xml) throws XmlPrettifyException {
-        return "";
-//        try {
-//            return "";
-//            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-//            Document doc = db.parse(new InputSource(new StringReader(xml)));
-//            OutputFormat format = new OutputFormat(doc);
-//            format.setIndenting(true);
-//            format.setIndent(2);
-//            format.setLineWidth(Integer.MAX_VALUE);
-//            Writer outxml = new StringWriter();
-//            XMLSerializer serializer = new XMLSerializer(outxml, format);
-//            serializer.serialize(doc);
-//            return outxml.toString();
-//        } catch (SAXException | IOException | ParserConfigurationException e) {
-//            throw new XmlPrettifyException(e.getMessage());
-//        }
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        transformerFactory.setAttribute("indent-number", 2);
+
+        try {
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        StringWriter stringWriter = new StringWriter();
+        StreamResult xmlOutput = new StreamResult(stringWriter);
+
+        Source xmlInput = new StreamSource(new StringReader(xml));
+        transformer.transform(xmlInput, xmlOutput);
+        return xmlOutput.getWriter().toString();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+            throw new XmlPrettifyException(e.getMessage());
+        }
     }
 }
